32fc63c10ed8168994146108309497163877bbcf
takahito.miyamoto <takahito@MacBook-Pro.local>
2020-07-11 20:33:13 +0900

    [modify] :sparkles: specificationPartner

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
  * @description index
  */
 import * as soap from 'soap';
+import safeStringify from 'fast-safe-stringify';
 import { authorization } from 'heat-sfdx-common';
 
 /**
@@ -19,4 +20,16 @@ async function methodsPartner(params: authorization) {
   return result;
 }
 
-export { methodsPartner };
+/**
+ * @name specificationPartner
+ * @description specification of Partner WSDL
+ */
+async function specificationPartner(params: authorization) {
+  const partnerWsdl = params.options.wsdl.partner;
+  const partnerClient = await soap.createClientAsync(partnerWsdl);
+  const partnerMethod = partnerClient.describe();
+  const result: string = safeStringify(partnerMethod.SforceService.Soap);
+  return result;
+}
+
+export { methodsPartner, specificationPartner };
